8732d230841d81772691701eef88409797cb2019
ADRIEN MARINI <adrien.marini@viacesi.fr>
2019-02-19 21:38:52 +0100

    ajout de la fonction de mémorisation du nouveau positionnement

--- resultTreatment.py
+++ resultTreatment.py
@@ -12,6 +12,10 @@ class resultTreatment():
 
     def calculMoveResult():
 
+        #vérification que le mouvement est autorisé (un serpent ne se mord pas la queue !)
+        #=> traité comme les autres collisions actuellement ; ci-dessous car le snake fait partie du dico
+
+        #recherche de collision avec d'autre snake
         if resultObjet.alive == True:
             try:
                 for snake in dicoResultObjet:
@@ -20,16 +24,25 @@ class resultTreatment():
                         if resultObjet.pos[0][0] == p[0][0] and resultObjet.pos[0][1] == p[0][1]:
                             resultObjet.alive = False
             except:
-                print("Erreur dans le calcul de collisions avec les autres snakes")
+                print("Erreur : calcul de collisions avec les autres snakes")
 
+        #recherche de collision avec un mur
         if resultObjet.alive == True :
             try:
                 for case in arene:
                     if case[resultObjet.pos[0][0]][resultObjet.pos[0][1]] == 1:
                         resultObjet.alive = False
             except:
-                print("Erreur dans le calcul de collisions avec les murs")
+                print("Erreur : calcul de collisions avec les murs")
 
+    def memoriseMove(indexResultObjet):
+
+        if resultObjet.alive == True:
+            try:
+                dicoObjetResult[indexResultObjet] = resultObjet
+
+            except:
+                print("Erreur : mémorisation du nouveau positionnement")
 pass
 
 
