f9dde756cfb97b6f6b7266ec8dd51b4c0ba6576a
ADRIEN MARINI <adrien.marini@viacesi.fr>
2019-02-19 15:50:14 +0100

    constructeur de l'objet result

--- resultObjet.py
+++ resultObjet.py
@@ -1,4 +1,39 @@
 class my_class(object):
+
+    #Constructeur
+    def __init__(self, name, position, alive, score):
+
+        self.name = name
+        self.position = position
+        self.alive = alive
+        self.score = score
+
+    
+
+
+    #"""EXEMPLE"""
+    #{
+    #    "snake1":{
+    #      "pos" : [
+    #        [1,2],
+    #        [1,3],
+    #        [1,4],
+    #        [1,5],
+    #        [1,6]
+    #        ],
+    #      "alive" : true,
+    #      "score" : 654
+    #      },
+    #    "snake2":{
+    #      "pos" : [
+    #        [2,2],
+    #        [2,3],
+    #        ],
+    #      "alive" : true,
+    #      "score" : 6969
+    #    }
+    #}
+
     pass
 
 
